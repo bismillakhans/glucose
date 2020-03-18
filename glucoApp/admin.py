8d66a24b85e9c72c48562d476fbffc33b23482f7
bismillakhans <bismillakhans@gmail.com>
2020-03-18 16:04:31 +0530

    error fixes 2

--- glucoApp/admin.py
+++ glucoApp/admin.py
@@ -1,11 +1,29 @@
+import csv
+
 from django.contrib import admin
 
 # Register your models here.
+from django.http import HttpResponse
+
 from glucoApp.models import Experiment
 
 
 class ExperimentModalAdmin(admin.ModelAdmin):
-    pass
+    actions = ["export_as_csv"]
 
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
 
 admin.site.register(Experiment, ExperimentModalAdmin)
